bdc048c0513205f316bcacea2f177d4a8eed4c43
Jyotiraditya <imjyotiraditya@pm.me>
2022-02-26 16:14:30 +0100

    Settings: Move blur toggle to Display options.
    Change-Id: I40836a814418bffdbf9500ae30027bf343e3d540

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -23,6 +23,7 @@ import android.os.Bundle;
 import com.android.settings.dashboard.DashboardFragment;
 import com.android.settings.display.BrightnessLevelPreferenceController;
 import com.android.settings.display.CameraGesturePreferenceController;
+import com.android.settings.display.EnableBlursPreferenceController;
 import com.android.settings.display.LiftToWakePreferenceController;
 import com.android.settings.display.OverlayCategoryPreferenceController;
 import com.android.settings.display.ScreenSaverPreferenceController;
@@ -88,6 +89,7 @@ public class DisplaySettings extends DashboardFragment {
         controllers.add(new ShowOperatorNamePreferenceController(context));
         controllers.add(new ThemePreferenceController(context));
         controllers.add(new BrightnessLevelPreferenceController(context, lifecycle));
+        controllers.add(new EnableBlursPreferenceController(context));
         controllers.add(new OverlayCategoryPreferenceController(context,
                 "android.theme.customization.font"));
         controllers.add(new OverlayCategoryPreferenceController(context,
