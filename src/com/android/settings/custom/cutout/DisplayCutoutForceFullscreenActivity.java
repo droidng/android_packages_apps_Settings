crdroidandroid/android_packages_apps_crDroidSettings

Rework cutout force fullscreen pref
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

src/com/crdroid/settings/fragments/ui/DisplayCutoutForceFullscreenActivity.java
@@ -1,0 +1,30 @@
+/*
+ * Copyright (C) 2022 crDroid Android Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.crdroid.settings.fragments.ui;
+
+import android.os.Bundle;
+import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
+import com.android.settingslib.collapsingtoolbar.R;
+
+public class DisplayCutoutForceFullscreenActivity extends CollapsingToolbarBaseActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getFragmentManager().beginTransaction()
+                .replace(R.id.content_frame, new DisplayCutoutForceFullscreenFragment())
+                .commit();
+    }
+}

droidng/android_packages_apps_Settings

src/com/android/settings/custom/cutout/DisplayCutoutForceFullscreenActivity.java
@@ -1,0 +1,30 @@
+/*
+ * Copyright (C) 2022 crDroid Android Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.settings.custom.cutout;
+
+import android.os.Bundle;
+import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
+import com.android.settingslib.collapsingtoolbar.R;
+
+public class DisplayCutoutForceFullscreenActivity extends CollapsingToolbarBaseActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getFragmentManager().beginTransaction()
+                .replace(R.id.content_frame, new DisplayCutoutForceFullscreenFragment())
+                .commit();
+    }
+}